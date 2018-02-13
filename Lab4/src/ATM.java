49f6ba9d44f2103e9d456ae2ac493e7246e859d5
mikelon4 <mikelon4@ceas-942-mac03.cae.uwm.edu>
2018-02-13 12:06:03 -0600

    Start on card, atm, and bank

--- Lab4/src/ATM.java
+++ Lab4/src/ATM.java
@@ -1,4 +1,40 @@
+import java.util.Scanner;
+
 
 public class ATM {
-
+	
+	private Account account;
+	private int accountNumber;
+	
+	public ATM() {
+		
+	}
+	
+	public ATM(Card card) {
+		this.accountNumber = card.getAccountNumber();
+		this.account = Bank.validate(accountNumber);
+		start();
+	}
+	
+	public void start() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter your pin...");
+		int pin = input.nextInt();
+		
+		if (pin != account.getPin()) {
+			System.out.println("You have entered an incorrect pin.");
+		} else {
+			System.out.println("What would you like to do? Withdraw = W, Deposit = D");
+			String choice = input.next();
+		}
+		
+	}
+	
+	public void withdraw(int amount) {
+		
+	}
+	
+	public void deposit(int amount) {
+		
+	}
 }
--- Lab4/src/Bank.java
+++ Lab4/src/Bank.java
@@ -1,4 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Bank {
+	
+	private Map<Account, Integer> accountList = new HashMap<>();
+	
+	public Bank() {
+		
+	}
+	
+	public static Account validate(int accountNumber) {
+		return null;
+	}
 
 }
