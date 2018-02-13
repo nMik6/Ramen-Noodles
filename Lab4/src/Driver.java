d4ec09388a763d05b59ab82a4f136b408a7ba6c5
Skyler <Skyler@10.22.78.90>
2018-02-13 12:33:15 -0600

    Starting driver method.

--- Lab4/src/Driver.java
+++ Lab4/src/Driver.java
@@ -1,4 +1,16 @@
 
 public class Driver {
+	
+	
+	public static void main() {
+	Bank bank = new Bank();
+	
+	Account first = new Account(1234, 6789, 80);
+	Account second = new Account(6789, 4321, 20);
+	
+	bank.addAccount(first);
+	bank.addAccount(second);
+	
+	}
 
 }
