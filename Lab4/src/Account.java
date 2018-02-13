5aeb5095ff1c818b788b1656094ffe79e59c49b6
Acer <Acer@10.22.60.132>
2018-02-13 11:46:24 -0600

    Account class framework

--- Lab4/src/Account.java
+++ Lab4/src/Account.java
@@ -1,4 +1,24 @@
 
 public class Account {
-
+	int accountNumber;
+	int PIN;
+	double balance;
+	
+	public Account() {
+		accountNumber = 0;
+		PIN = 0;
+		balance = 0;
+	}
+	
+	public Account(int a,int p) {
+		accountNumber = a;
+		PIN = p;
+		balance = 0;
+	}
+	
+	public Account(int a,int p, double b) {
+		accountNumber = a;
+		PIN = p;
+		balance = b;
+	}
 }
