529244f2051d79cdad57fe50a1644152a925fdbf
Acer <Acer@LAPTOP-925OLBI6>
2018-02-23 19:44:33 -0600

    SimulaterTest getting started

--- Lab5/src/src/test/SimulatorTest.java
+++ Lab5/src/src/test/SimulatorTest.java
@@ -1,5 +1,21 @@
 package src.test;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import src.main.Account;
+import src.main.ATM;
+import src.main.Bank;
+import src.main.Card;
+import src.main.CashDispenser;
+import src.main.Printer;
+import src.main.Simulator;
+
+/*
+ * Written by: Thomas Crownover
+ * 
+ */
 public class SimulatorTest {
 
 }
