17de25b05cbe1c2227fb63889715f09a69aaa987
swkeo <swkeo@DESKTOP-CINILAR>
2018-02-24 17:45:18 -0600

    Made first pass on Racer and Logger, taking a break then will look again

--- Sprint 1/src/src/main/Logger.java
+++ Sprint 1/src/src/main/Logger.java
@@ -1,5 +1,36 @@
 package src.main;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.JSONObject;		//Not sure how this works yet, working on it.
 
-public class Logger {
+public class Logger {	
+	//BufferedWriter's take a writer of some sort as a parameter, so I chose the FileWriter. We can change that if people would like.
+	
+	
+	private BufferedWriter raceBuffWriter;
+	private BufferedWriter debugBuffWriter;
+	private FileWriter raceFileWriter;
+	private FileWriter debugFileWriter;
+	
+	private JSONObject jsonRace;
+	
+	
+	public Logger(String debugFileName, String raceJsonFileName)
+	{
+		try {	//Not sure if this is necessary or the best way to do this but It's probably necessary.
+			debugFileWriter = new FileWriter(debugFileName);
+			raceFileWriter = new FileWriter(raceJsonFileName);
+			
+			debugBuffWriter = new BufferedWriter(debugFileWriter);
+			raceBuffWriter = new BufferedWriter(raceBuffWriter);
+			
+		} catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 }
--- Sprint 1/src/src/main/Racer.java
+++ Sprint 1/src/src/main/Racer.java
@@ -1,5 +1,44 @@
 package src.main;
 
 public class Racer {
+	
+	private int bib;
+	private Time start;
+	private Time finish;
+	private Time total;
+	private boolean dnf;
+	
+	public Racer(int bib) {
+	
+		this.bib = bib;
+		dnf = false;
+	}
+	
+	public int start(Time t) {	//Is 1 what we should return if correct?
+	
+		start = t;
+		return 1;
+	}
+	
+	public int finish(Time t) {
+	
+		finish = t;
+		return 1;
+		
+	}
+	
+	public boolean isRacing() {
+	
+		if (this.start != null && this.finish == null && !dnf) //If the race has started, but hasn't finished and wasn't DQ'd, then they must be racing.
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public Time getTotal() {
+	//Either I will call out to Time to make the calculations between start and finish, or I will do the calculation here, but that seems incorrect.
+		return this.total;
+	}
 
 }
