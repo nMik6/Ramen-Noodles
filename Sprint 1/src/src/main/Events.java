f909b0e40eee2a5655fd5d029e8d78c2a75bed0b
miklon <miklon@10.22.188.234>
2018-02-24 13:38:39 -0600

    forgot return statements (done for no errors right now)

--- Sprint 1/src/src/main/Events.java
+++ Sprint 1/src/src/main/Events.java
@@ -5,47 +5,47 @@ import java.time.LocalTime;
 public class Events {
 	
 	public int power() {
-		
+		return -1;
 	}
 	
 	public int exit() {
-		
+		return -1;
 	}
 	
 	public int reset() {
-		
+		return -1;
 	}
 	
 	public int time(LocalTime time, Racer racer) {
-		
+		return -1;
 	}
 	
 	public int dnf(Racer racer) {
-		
+		return -1;
 	}
 	
 	public int cancel(Racer racer) {
-		
+		return -1;
 	}
 	
 	public int tog(int channel) {
-		
+		return -1;
 	}
 	
 	public int trig(int channel) {
-		
+		return -1;
 	}
 	
 	public int newRun() {
-		
+		return -1;
 	}
 	
 	public int start(LocalTime time, Racer racer) {
-		
+		return -1;
 	}
 	
 	public int finish(LocalTime time, Racer racer) {
-		
+		return -1;
 	}
 
 }
