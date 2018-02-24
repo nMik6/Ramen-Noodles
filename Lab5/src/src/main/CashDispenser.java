92becf95d90ba313a578d561b4f5bcce7fd10a38
swkeo <swkeo@DESKTOP-CINILAR>
2018-02-23 21:55:51 -0600

    CashDispenser documetation

--- Lab5/src/src/main/CashDispenser.java
+++ Lab5/src/src/main/CashDispenser.java
@@ -1,7 +1,7 @@
 package src.main;
 //Created by: Skyler
 //Cash Dispenser object to dispense the cash that the user has withdrawn from the ATM. Would map to a physical ATM dispenser if it was a real ATM
-//A physical dispenser could actually fail due to mechanical error or being out of cash, but our simulated ATM cannot fail.
+//Our ATM has a total cash amount that cannot be exceeded.
 
 //CashDispenser object to dispense cash.
 public class CashDispenser {
