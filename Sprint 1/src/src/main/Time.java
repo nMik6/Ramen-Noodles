83c78ef301e128fd73ae053eba6a77a11f03de39
miklon <miklon@10.22.188.234>
2018-02-24 13:22:43 -0600

    Time class is done a little differently, open for suggestions

---
+++ Sprint 1/src/src/main/Time.java
@@ -0,0 +1,36 @@
+package src.main;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Time {
+	
+	private LocalTime time;
+	
+	public Time() {
+		time = LocalTime.now();
+	}
+	
+	public Time(LocalTime time) {
+		this.time = time;
+	}
+	
+	public Time(String time) {
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss");
+		this.time = LocalTime.parse(time, format);
+	}
+	
+	public String timeDifference(Time one, Time two) {
+		long hour = ChronoUnit.HOURS.between(one.getTime(), two.getTime());
+		long minute = ChronoUnit.MINUTES.between(one.getTime(), two.getTime());
+		long second = ChronoUnit.SECONDS.between(one.getTime(), two.getTime());
+		long milli = ChronoUnit.MILLIS.between(one.getTime(), two.getTime());
+		return hour + ":" + minute + ":" + second + ":" + milli;
+	}
+	
+	public LocalTime getTime() {
+		return time;
+	}
+
+}
