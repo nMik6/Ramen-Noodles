dd5f9dce853dc26e7b24b0a3a2038070d96fdf24
miklon <miklon@10.22.188.234>
2018-02-24 13:37:36 -0600

    Basic of events, not entirely sure on how we want to handle all of this yet.

--- Sprint 1/src/src/main/Events.java
+++ Sprint 1/src/src/main/Events.java
@@ -1,5 +1,51 @@
 package src.main;
 
+import java.time.LocalTime;
+
 public class Events {
+	
+	public int power() {
+		
+	}
+	
+	public int exit() {
+		
+	}
+	
+	public int reset() {
+		
+	}
+	
+	public int time(LocalTime time, Racer racer) {
+		
+	}
+	
+	public int dnf(Racer racer) {
+		
+	}
+	
+	public int cancel(Racer racer) {
+		
+	}
+	
+	public int tog(int channel) {
+		
+	}
+	
+	public int trig(int channel) {
+		
+	}
+	
+	public int newRun() {
+		
+	}
+	
+	public int start(LocalTime time, Racer racer) {
+		
+	}
+	
+	public int finish(LocalTime time, Racer racer) {
+		
+	}
 
 }
