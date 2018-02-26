f94b15bb4839165b534f9b9fc1d5d7af3b535b4c
mikel <mikel@192.168.0.7>
2018-02-26 09:02:49 -0600

    Some additions to Race. Need clarification on the actual difference between TOG and TRIG

--- Sprint 1/src/src/main/Race.java
+++ Sprint 1/src/src/main/Race.java
@@ -7,6 +7,7 @@ import java.util.Queue;
 import java.util.ArrayDeque;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 
@@ -16,7 +17,7 @@ public class Race {
 	private String lastCommand;
 	private Queue<Racer> readyRacers;
 	private Queue<Racer> curRacers;
-	private ArrayList<Racer> finishRacers;
+	private List<Racer> finishRacers;
 	private boolean[] channels;
 	private boolean raceFinished;
 	
@@ -26,15 +27,20 @@ public class Race {
 		this.channels[1] = this.channels[2] = false;
 		this.curRacers = new ArrayDeque<Racer>();
 		this.finishRacers = new ArrayList<Racer>();
-		this.raceFinished = false;
-		
+		this.raceFinished = false;	
 	}
 
-	/*
-	 * public to test, otherwise create a getter for testing purposes.
-	*/
-	public Queue<Racer> readyRacers = new LinkedList<Racer>(); 
-
+	public Queue<Racer> getReadyRacers() {
+		return readyRacers;
+	}
+	
+	public Queue<Racer> getCurrentRacers() {
+		return curRacers;
+	}
+	
+	public List<Racer> getFinishedRacers() {
+		return finishRacers;
+	}
 	
 	public int power() {
 		return -1;
@@ -48,23 +54,36 @@ public class Race {
 		return -1;
 	}
 	
-	public int time(LocalTime time, Racer racer) {
+	public int time(Time time, Racer racer) {
 		return -1;
 	}
 	
+	/**
+	 * Assigns the DNF flag to the racer
+	 * @param racer
+	 * @return 1 if the assignment is successful, and -1 otherwise
+	 */
 	public int dnf(Racer racer) {
-		return -1;
+		return racer.dnf() == 1 ? 1 : -1;
 	}
 	
 	public int cancel(Racer racer) {
 		return -1;
 	}
 	
+	/**
+	 * Toggles the state of the channel
+	 * @param channel index of the channel to toggle
+	 * @return 1 if channel is toggled and -1 if @param is greater than
+	 * the number of channels
+	 */
 	public int tog(int channel) {
-		return -1;
+		if (channel > 2) return -1;
+		channels[channel] = !channels[channel];
+		return 1;
 	}
 	
-	public int trig(int channel) {
+	public int trig(int channel) { // Unsure of the difference between this and tog is supposed to be
 		return -1;
 	}
 	
@@ -72,10 +91,22 @@ public class Race {
 		return -1;
 	}
 	
+	/**
+	 * Assigns the start time to the racer
+	 * @param time at which the racer starts
+	 * @param racer
+	 * @return 1 if successful and -1 otherwise
+	 */
 	public int start(Time time, Racer racer) {
 		return (racer.start(time) == 1) ? 1 : -1;
 	}
 	
+	/**
+	 * Assigns the racers finishing time
+	 * @param time that the racer finishes at
+	 * @param racer
+	 * @return 1 if successful and -1 otherwise
+	 */
 	public int finish(Time time, Racer racer) {
 		return (racer.finish(time) == 1) ? 1 : -1;
 	}
