8c9fbbf64b62959c890b58a348412c3586a85264
Acer <Acer@10.22.61.62>
2018-02-27 11:57:29 -0600

    Fixing formatting issues in RacerTest

--- Sprint 1/src/src/test/RacerTest.java
+++ Sprint 1/src/src/test/RacerTest.java
@@ -23,7 +23,7 @@ class RacerTest {
 	
 	//Creates Racer, starts and finishes the racer, ensuring a basic run will succeed.
 	@Test
-	void activeRacer() {
+	public void activeRacer() {
 		assertFalse(testRacer.isRacing(), "Racing without start");
 		assertEquals(1, testRacer.start(testTime),"Racer has started");
 		assertTrue(testRacer.isRacing(), "Racer should be racing");
@@ -34,7 +34,7 @@ class RacerTest {
 	}
 	//Tests did not finish.
 	@Test
-	void racerDNF() {
+	public void racerDNF() {
 		assertFalse(testRacer.isRacing(), "Racing without start");
 		assertEquals(1, testRacer.start(testTime),"Racer has started");
 		assertTrue(testRacer.isRacing(), "Racer should be racing");
@@ -45,11 +45,11 @@ class RacerTest {
 	}
 	
 	@Test
-	void getTotalEarly() {
+	public void getTotalEarly() {
 		assertEquals(1, testRacer.start(testTime), "Racer starting now");
 		assertTrue(testRacer.isRacing(), "Racer should be racing");
 		assertEquals(null, testRacer.getTotal(), "Cannot get total time because Racer is still racing!");
 		assertEquals(1, testRacer.finish(testTimeFinish), "Racer has finished!");
 		assertEquals(testRacer.getTotal().getClass(), testTime.getClass(),"Get Total returned a time object");
-
+	}
 }
