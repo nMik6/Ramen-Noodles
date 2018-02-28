7cb8cdab5a897b521fd46f80cc1646afcf59d774
mikel <mikel@192.168.0.7>
2018-02-27 20:09:41 -0600

    Difference method should work properly now

--- Sprint 1/src/src/main/Time.java
+++ Sprint 1/src/src/main/Time.java
@@ -7,7 +7,8 @@ import java.time.temporal.ChronoUnit;
 public class Time {
 	
 	private LocalTime time;
-	private transient DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+	private transient static DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss.S");
+
 	
 	public Time() {
 		time = LocalTime.now();
@@ -37,22 +38,14 @@ public class Time {
 		return new Time(this.time.plusHours(amount));
 	}
 	
-	public static void main(String[] args) {
-		Time time = new Time(LocalTime.now());
-		System.out.println(time.getTime());
-		time = time.addSeconds(15);
-		System.out.println(time.getTime());
-		time = time.addMinutes(63);
-		System.out.println(time.getTime());
-		time = time.addHours(5);
-		System.out.println(time.getTime());
-	}
-	
 	public Time difference(Time one, Time two) {
-		long hour = ChronoUnit.HOURS.between(one.getTime(), two.getTime());
-		long minute = ChronoUnit.MINUTES.between(one.getTime(), two.getTime());
-		long second = ChronoUnit.SECONDS.between(one.getTime(), two.getTime());
-		long milli = ChronoUnit.MILLIS.between(one.getTime(), two.getTime());
+		String hour = Long.toString(ChronoUnit.HOURS.between(one.getTime(), two.getTime()));
+		String minute = Long.toString(ChronoUnit.MINUTES.between(one.getTime(), two.getTime()));
+		String second = Long.toString(ChronoUnit.SECONDS.between(one.getTime(), two.getTime()));
+		String milli = Long.toString(ChronoUnit.MILLIS.between(one.getTime(), two.getTime()));
+		if (hour.length() < 2) hour = "0" + hour;
+		if (minute.length() < 2) minute = "0" + minute;
+		milli = milli.substring(0, 1);
 		return new Time(LocalTime.parse(hour + ":" + minute + ":" + second + "." + milli, format));
 	}
 	
