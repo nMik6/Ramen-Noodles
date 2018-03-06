745220d31b80b1b90c8e26ebb485d58e1416b275
miklon <miklon@10.22.92.88>
2018-03-06 10:14:23 -0600

    employee files

--- Lab7/src/src/editor/Employee.java
+++ Lab7/src/src/editor/Employee.java
@@ -1,5 +1,21 @@
 package src.editor;
 
 public class Employee {
-
+	
+	private String firstName;
+	private String lastName;
+	private String department;
+	private String phoneNumber;
+	
+	public Employee(String firstName, String lastName, String department, String phoneNumber) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.department = department;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String toString() {
+		return firstName + ", " + lastName + " " + phoneNumber + " " + department;
+	}
 }
