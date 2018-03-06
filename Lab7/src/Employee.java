5b0bd5917d9389263bd034e63b01ad9a94c77071
miklon <miklon@10.22.92.88>
2018-03-06 11:25:05 -0600

    employee

--- Lab7/src/Employee.java
+++ Lab7/src/Employee.java
@@ -1,4 +1,21 @@
 
 public class Employee {
 
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String department;
+	
+	public Employee(String firstName, String lastName, String phoneNumber, String department) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.department = department;
+	}
+	
+	public String toString() {
+		return firstName + ". " + lastName + " " + phoneNumber + " " + department;
+	}
+	
 }
