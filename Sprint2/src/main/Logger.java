04cede53b35accd63ee6715960c7be6c45c506c4
hartmut <hartmut@uwm.edu>
2018-03-13 12:14:12 -0500

    logger print export work

--- Sprint2/src/main/Logger.java
+++ Sprint2/src/main/Logger.java
@@ -36,7 +36,7 @@ public class Logger {
 	 */
 	public void print(List<Racer> racers) {
 		for(Racer r: racers) {
-			System.out.printf("Name: %d\tStart: %s, Finish: %s, Total: %s", r.getName(), r.getStart().printTime(), r.getFinish().printTime(), r.getTotal().printTime());
+			System.out.printf("Name: %d\tStart: %s, Finish: %s, Total: %s\n", r.getName(), r.getStart().printTime(), r.getFinish().printTime(), r.getTotal().printTime());
 		}
 	}
 	
