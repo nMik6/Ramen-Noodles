196c891d7f7be490881fa8d20e8d436dac3f50b1
mikel <mikel@192.168.0.7>
2018-04-16 12:37:57 -0500

    methods added

--- Sprint3/src/main/events/Event.java
+++ Sprint3/src/main/events/Event.java
@@ -1,10 +1,11 @@
 package main.events;
 
 import java.time.format.DateTimeFormatter;
+
+import main.Channel;
 import main.RaceData;
 import main.racing.Racer;
 
-import java.time.LocalTime;
 import java.time.format.FormatStyle;
 
 import main.Time;
@@ -18,13 +19,45 @@ public class Event {
 	
 	public void power() {
 		RaceData.switchPower();
+		if (RaceData.isPower())
+			for (int i = 0; i < 7; i++) {
+				RaceData.setChannel(i, new Channel());
+			}
 	}
 	
 	public void exit() {
 		
 	}
 	
+	public void reset() {
+		if (!RaceData.isPower()) return;
+		RaceData.switchPower();
+		RaceData.setCurrentRace(null);
+		RaceData.setCommand("none");
+		RaceData.setOffset(false);
+		for (int i = 0; i < 7; i++) {
+			RaceData.setChannel(i, new Channel());
+		}
+	}
+	
+	public void print() {
+		if (!RaceData.isPower() || RaceData.getCurrentRace() == null)
+			return;
+		
+		for (Racer r: RaceData.getCurrentRace().getFinishedRacers()) {
+			if (!r.didNotFinish()) {
+			System.out.printf("Racer: %d,\tStart: %s,\tFinish: %s,\tTotal: %s\n", 
+					r.getName(), r.getStart().printTime(), r.getFinish().printTime(), r.getTotal().printTime());
+			}
+			else {
+				System.out.printf("Racer: %d,\tStart: %s, \tDid not finish!", 
+						r.getName(), r.getStart().printTime());
+			}
+		}
+	}
+	
 	public void conn (String one, String two) {
+		if (!RaceData.isPower()) return;
 		
 	}
 	
@@ -59,7 +92,5 @@ public class Event {
 	public void export(int run) { //Why does this take an int? 
 		
 	}
-	
-	
 
 }
